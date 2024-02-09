566ad4091df21e60edbe3af0a1dc1e99faeaa10a
manon-rittling <manon.rittling@laplateforme.io>
2024-02-09 13:00:12 +0100

    class mydb fini avec connexion serveur et crud global

--- class/MyDb.py
+++ class/MyDb.py
@@ -33,9 +33,45 @@ class MyDb:
         self.deconnexion()
         return result 
     
+    def create(self, table, fields, values):
+        requete = "INSERT INTO " + table + " (" + fields + ") VALUES (" + values + ")"
+        self.executeRequete(requete)
+
+    def read(self, table, fields, condition):
+        requete = "SELECT " + fields + " FROM " + table + " WHERE " + condition
+        return self.fetch(requete)
+    
+    def update(self, table, fields, condition):
+        requete = "UPDATE " + table + " SET " + fields + " WHERE " + condition
+        self.executeRequete(requete)
+
+    def delete(self, table, condition):
+        requete = "DELETE FROM " + table + " WHERE " + condition
+        self.executeRequete(requete)
+
+
 # test code
 db = MyDb("82.165.185.52", "marijo", "Rijoma13!", "manon-rittling_mydiscord")
 db.connexion()
+
+# table = "chatRoom"
+# fields = "name, type_room"
+# values = "'chatRoom2', 'private'"
+# db.create(table, fields, values)
+
+# table = "chatRoom"
+# fields = "name = 'chatRoom1'"
+# condition = "id_room = 1"
+# db.update(table, fields, condition)
+
+# table = "chatRoom"
+# condition = "id_room = 3"
+# db.delete(table, condition)
+
+
+
+db.deconnexion()
+
 
 
 
