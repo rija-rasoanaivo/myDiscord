d37eba4adb99369876d61d6fde46227453d59af0
jordan-reinaldo <jordan.reinaldo@laplateforme.io>
2024-02-15 12:22:01 +0100

    modification class message suppression de l'import de Login dans cette dernière

--- class/backend/Message.py
+++ class/backend/Message.py
@@ -1,6 +1,5 @@
 from Server import Server
 from datetime import datetime
-from Login import Login
 
 class Message:
 
@@ -20,24 +19,4 @@ class Message:
         """
         
         # Execute the query
-        self.db.executeRequete(query, (self.user_id, self.id_room, message_content, timestamp))
-        
-
-# This would be used after a successful login and joining a room
-if __name__ == "__main__":
-    user_manager = Login()
-    login_success, user_id = user_manager.login()
-
-    if login_success:
-        print(f"User ID is: {user_id}")
-        
-        # Assume that the user has joined a room and you have the room's ID
-        id_room = input("Enter the id_room you have joined: ")
-        
-        message_client = Message(user_id, id_room)
-        
-        # Example of sending a message
-        message_content = input("Enter your message: ")
-        message_client.send_message(message_content)
-
-    
+        self.db.executeRequete(query, (self.user_id, self.id_room, message_content, timestamp))  
