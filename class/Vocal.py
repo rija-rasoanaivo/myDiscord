415f1663a888dd376e678d6b12318880e267b420
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2024-02-19 18:03:36 +0100

    push de sauvegarde

--- class/Vocal.py
+++ class/Vocal.py
@@ -2,11 +2,12 @@ import sounddevice as sd
 import base64
 import numpy as np
 from scipy.io.wavfile import read, write
+from Chatting import *
 
 class Vocal:
     
     def __init__(self):
-        pass
+        self.db = Server.db
 
     def record_audio(self, filename="recording.wav", duration=5):
         # Enregistrement de l'audio
@@ -20,24 +21,21 @@ class Vocal:
     def convert_into_binary(self, filename):
         # Conversion de l'audio en données binaires
         audio_array = np.array(read(filename)[1], dtype=float)
+        audio_array /= np.max(np.abs(audio_array))  # Normalisation entre -1 et 1
         audio_bytes = audio_array.tobytes()
-        return base64.b64encode(audio_bytes).decode("utf-8")
+        return audio_bytes
     
-    def decode_from_binary(self, binary_data):
-        # Décodage des données binaires en audio
-        audio_bytes = base64.b64decode(binary_data)
-        audio_array = np.frombuffer(audio_bytes, dtype=float)
-        return audio_array
-
-    def play_audio(self, filename):
-        # Lecture de l'audio
-        freq, audio_array = read(filename)
-        sd.play(audio_array, freq)
-        sd.wait()
+    def insert_audio_into_db(self, name, audio_binary, type_room):
+        # Insérer l'audio dans la base de données
+        sql = "INSERT INTO vocalChatRoom (name, vocal_message, type_room) VALUES (%s, %s, %s)"
+        values = (name, audio_binary, type_room)
+        self.db.executeRequete(sql, values)
+        self.db.fetch()
 
 if __name__ == "__main__":
     vocal = Vocal()
     vocal.record_audio()
     binary_data = vocal.convert_into_binary("recording.wav")
-    audio_array = vocal.decode_from_binary(binary_data)
-    vocal.play_audio(audio_array)
+
+    # Insérer l'audio dans la base de données
+    vocal.insert_audio_into_db("Recording", binary_data, "room_type")
