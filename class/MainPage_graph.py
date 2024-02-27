6ffbf9f0ea54522abc868089be8cf38892c8b3f5
manon-rittling <manon.rittling@laplateforme.io>
2024-02-27 18:04:43 +0100

    debogage bouton deco retour page login

--- class/MainPage_graph.py
+++ class/MainPage_graph.py
@@ -301,6 +301,8 @@ class MainPage_graph(Tk):
 
     def returnPageLogin(self):
         try:
+            from Login_graph import Login_graph  # Import tardif
+            
             # Libérer les ressources si nécessaire
             self.voice_thread = None
             
@@ -310,12 +312,7 @@ class MainPage_graph(Tk):
             # Créer une nouvelle instance de la page de connexion
             go_login = Login_graph()
             go_login.mainloop()
-            
-            # Mettre à jour l'interface si nécessaire
-            go_login.update()
 
-            self.update()
-            
         except Exception as e:
             print("Une erreur s'est produite lors du retour à la page de connexion:", e)
         
