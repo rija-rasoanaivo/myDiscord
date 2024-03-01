10ff241281a06a386f44e6b49477012cc3b37a70
manon-rittling <manon.rittling@laplateforme.io>
2024-03-01 12:15:16 +0100

    commentaire en anglaisen cours

--- class/MainPage_graph.py
+++ class/MainPage_graph.py
@@ -96,10 +96,10 @@ class MainPage_graph(Tk):
                 button.place(x=80, y=50 + i * 50)
 
                 
-
+    # method for display create room
     def toggle_createRoom(self):
         
-        # Si la frame3 est déjà affichée, la faire disparaître et détruire ses éléments
+        # if the frame3 is displayed, hide it
         if self.frame3.winfo_ismapped():
             self.frame3.place_forget()
             self.titre.winfo_ismapped()
@@ -129,28 +129,31 @@ class MainPage_graph(Tk):
             
             self.frame3.place(x=400, y=150)
 
-            # Création du titre de la frame
+            # label title
             self.titre = ctk.CTkLabel(self, text="CREATE YOUR ROOM", width=20, height=20, font=('Broadway', 22), text_color="#c7c1f2", fg_color="#415059")
             self.titre.place(x=550, y=180, anchor=CENTER)
 
-            # Création du champ pour le nom du salon
+            # label room name
             self.roomName = ctk.CTkLabel(self, text="Room Name", width=20, height=20, font=('Agency FB', 18, 'bold'), text_color="#c7c1f2", fg_color="#415059")
             self.roomName.place(x=550, y=210, anchor=CENTER)
 
+            # entry room name
             self.entry_roomName = ctk.CTkEntry(self, width=100, height=30, corner_radius=5, fg_color="white", bg_color="#415059", border_color="#38454c", border_width=1, text_color="black")
             self.entry_roomName.place(x=550, y=250, anchor=CENTER)
 
-            # Création de la checkbox pour choisir salon privé ou public
+            # label type room
             self.type_room = ctk.CTkLabel(self, text="Type Room", width=20, height=20, font=('Agency FB', 18, 'bold'), text_color="#c7c1f2", fg_color="#415059")
             self.type_room.place(x=550, y=280, anchor=CENTER)
 
+            # checkbox public
             self.checkPublic = ctk.CTkCheckBox(self, text="Public", text_color="white", width=40, height=20, bg_color="#415059", corner_radius=5, border_color="white", border_width=1)
             self.checkPublic.place(x=480, y=310, anchor=CENTER)
 
+            # checkbox private
             self.checkPrivate = ctk.CTkCheckBox(self, text="Private", text_color="white", width=40, height=20, bg_color="#415059", corner_radius=5, border_color="white", border_width=1)
             self.checkPrivate.place(x=630, y=310, anchor=CENTER)
             
-            # creation combobox pour ajouter les membres dans le salon
+            # create label add members
             self.members = ctk.CTkLabel(self, text="Add Members", width=20, height=20, font=('Agency FB', 18, 'bold'), text_color="#c7c1f2", fg_color="#415059")
             self.members.place(x=550, y=340, anchor=CENTER)
             private_chat_room = PrivateChatRoom()
@@ -160,21 +163,26 @@ class MainPage_graph(Tk):
             self.combo = ctk.CTkComboBox(self, width=150, height=25, corner_radius=5, fg_color="white", bg_color="#415059", border_color="#38454c", border_width=1, values=member_names)
             self.combo.place(x=550, y=370, anchor=CENTER)
 
-            # Création du bouton "valider"
+            # create button valid
             self.buttonValid = ctk.CTkButton(self, text="VALID", text_color="#38454c", width=80, height=20, corner_radius=10, font=("Agency FB", 21, "bold"), border_width=2, border_color="white", bg_color="#415059", fg_color="#c7c1f2", hover_color="#a78ff7", command=self.join_datacreateroom)
             self.buttonValid.place(x=550, y=420, anchor=CENTER)
     
+
+    # ------------ party frame4 (messages) ----------------
+
     # fermer la frame4
     def outRoombutton(self):
         if self.frame4.winfo_ismapped():
             self.frame4.place_forget()
             self.stop_refreshing_messages()
 
-
+    # method for display frame4
     def show_frame4(self):
         # Afficher la frame4
         self.frame4.place(x=300, y=0)
 
+
+    # method for display message
     def frame4_message(self, id_room):
         # Clear any previous messages displayed in frame4
         for widget in self.frame4.winfo_children():
@@ -188,7 +196,7 @@ class MainPage_graph(Tk):
         self.current_chat_instance = Chatting(self.user_id, id_room)
         messages = self.current_chat_instance.load_messages(id_room, self.user_id)
 
-        # bouton pour quitter le salon
+        # button to close the frame4
         self.ImageoutRoom = PhotoImage(file="image/boutons/outRoom1.png")
         self.buttonOutRoom = ctk.CTkButton(self.frame4, image=self.ImageoutRoom, text=None, width=20, height=20, fg_color="#23272d", hover_color="#23b0ed", corner_radius=10, command=lambda: self.outRoombutton())
         self.buttonOutRoom.place(x=450, y=10)
@@ -198,21 +206,19 @@ class MainPage_graph(Tk):
         if messages:
             for i, message in enumerate(messages):
                 message_text = f"{message[1]}"
-                message_label = ctk.CTkLabel(self.frame4, text=message_text, width=170, height=30, corner_radius=10,font=("Agency FB", 20, 'bold'), fg_color="#aeb8f9", bg_color="#23272d", text_color="black")
-                message_label.place(x=80, y=30 + i * 70)
-                
-                
+                message_label = ctk.CTkLabel(self.frame4, text=message_text, width=170, height=30, corner_radius=10,font=("Agency FB", 20, 'bold'), fg_color="#ccd2ff", bg_color="#23272d", text_color="black")
+                message_label.place(x=80, y=30 + i * 70)  
 
             for i, message in enumerate(messages):
                 message_text = f"{message[0]}  {message[2]} " 
                 self.messageDisplay = ctk.CTkLabel(self.frame4, text=message_text, width=100, height=20, font=("Agency FB", 13, 'bold'),text_color="white", bg_color="#23272d" )
                 self.messageDisplay.place(x=90, y=60 + i * 70)
-
-            
+  
         else:
             default_message = ctk.CTkLabel(self.frame4, text="No messages in this room.", width=200, height=20, corner_radius=10,font=("Agency FB", 18, 'bold'), fg_color="#aeb8f9", bg_color="#aeb8f9")
             default_message.place(x=80, y=50)
 
+    # method for initialize message input area
     def initialize_message_input_area(self):
 
         # Message entry textbox
@@ -247,78 +253,72 @@ class MainPage_graph(Tk):
             button.place(x=x_position, y=550, anchor=CENTER)
             x_position += 30  
 
-    def toggle_voice_message(self):
 
-        print("Toggle voice message method called")
-        if not self.recording:  # Si l'enregistrement n'est pas en cours, démarrez-le
+    
+    # ---------------------- party vocal ----------------------
+
+    # method for toggle voice message
+    def toggle_voice_message(self):
+        if not self.recording:  
             print("Starting voice message recording")
             self.start_voice_message()
-        else:  # Sinon, arrêtez l'enregistrement
+        else:  
             print("Stopping voice message recording")
             self.stop_voice_message()
 
         self.update()
 
-    # parti vocal 
+        
+    # method for start voice message 
     def start_voice_message(self):
         print("Starting voice message thread")
         self.recording = True
-        # Créer une instance de Vocal
         self.voice = Vocal()
-        # Démarrer le thread d'enregistrement vocal
         self.voice_thread = threading.Thread(target=self.start_voice_message_thread)
         self.voice_thread.start()
 
-
+    # method for stop voice message
     def stop_voice_message(self):
         print("Stopping voice message recording")
-        self.recording = False  # Mettre à jour l'état de l'enregistrement
-        # Appeler la méthode stop de l'instance de Vocal pour arrêter l'enregistrement
+        self.recording = False  
+        
         if self.voice:
             self.voice.stop()
-        # Si le thread d'enregistrement vocal est en cours, attendre qu'il se termine
+        
         if self.voice_thread is not None:
             self.voice_thread.join()
 
-    def start_voice_message_thread(self):
-        if self.recording:  # Vérifier que l'enregistrement est toujours actif
-            self.voice.start()
-            self.recording = False  # Mettre à jour l'état de l'enregistrement lorsque celui-ci est terminé
-    def start_server(self):
-        if not self.server_started:  # Vérifiez si le serveur n'est pas déjà démarré
-            print("Starting server thread")
-            self.server_thread = threading.Thread(target=self.server.start_server)
-            self.server_thread.daemon = True
-            self.server_thread.start()
-            self.server_started = True  # Mettez à jour l'état du serveur
-        else:
-            print("Stopping server thread")
-            self.server.stop_server()  # Arrêtez le serveur si c'est déjà démarré
-            self.server_started = False  # Mettez à jour l'état du serveur
 
+    # method for start voice message thread
+    def start_voice_message_thread(self):
+        if self.recording:  
+            self.voice.start()
+            self.recording = False  
+
+    # method for start server
     def start_server(self):
-        if not self.server_started:  # Vérifiez si le serveur n'est pas déjà démarré
-            print("Starting server thread")
+        if not self.server_started:  
+        
             self.server_thread = threading.Thread(target=self.server.start_server)
             self.server_thread.daemon = True
             self.server_thread.start()
-            self.server_started = True  # Mettez à jour l'état du serveur
+            self.server_started = True  
         else:
-            print("Stopping server thread")
-            self.server.stop_server()  # Arrêtez le serveur si c'est déjà démarré
-            self.server_started = False  # Mettez à jour l'état du serveur
+            
+            self.server.stop_server()  
+            self.server_started = False  
+
+
+    # ------------------- party interaction db -------------------     
 
     def returnPageLogin(self):
         try:
             from Login_graph import Login_graph  # Import tardif
             
-            # Libérer les ressources si nécessaire
             self.voice_thread = None
             
-            # Détruire la fenêtre actuelle
             self.destroy()
             
-            # Créer une nouvelle instance de la page de connexion
             go_login = Login_graph()
             go_login.mainloop()
 
@@ -326,8 +326,7 @@ class MainPage_graph(Tk):
             print("Une erreur s'est produite lors du retour à la page de connexion:", e)
         
 
-   
-     # methode pour creer un salon
+    # method join data to createRoom graph
     def join_datacreateroom(self):
         roomName = self.entry_roomName.get().strip()
         isPublic = self.checkPublic.get()
@@ -348,17 +347,16 @@ class MainPage_graph(Tk):
                     private_chat_room.admin_add_member_private_chat_room(selected_member_id, room_id)
                 
                 self.toggle_right_frame()
-        else:
-            print("Error creating the room.")
+                self.update()
 
-
+        
+    # method for select room
     def select_room(self, id_room):
-        
         room_type = self.chat_room.get_room_type(id_room)
-        room_type = str(room_type).strip() # Convertir en chaîne et supprimer les espaces
+        room_type = str(room_type).strip() # Convert to string and remove leading/trailing spaces
         
-
-        if room_type == '1': # Si le salon est privé, vérifiez l'autorisation de l'utilisateur
+        # verify if the room is private or public
+        if room_type == '1': 
             auth = self.private_chat_room1.get_user_authorization(self.user_id, id_room)
             print(f"User authorization: {auth}")
             if auth in ['admin', 'member']:
@@ -371,47 +369,39 @@ class MainPage_graph(Tk):
                 self.after(1000, labelDenied.destroy)
                 return
         else:
-            print("Access to public room.")
+            
             self.frame4_message(id_room)
             self.start_refreshing_messages()
         
-            
-
+    # method for send message
     def send_message(self):
-        # Récupère le contenu du ctk.CTkTextbox
+        
         message_content = self.text.get("1.0", "end-1c").strip()
         if message_content:
-            # Utilise l'instance de Chatting pour envoyer le message
+            
             self.current_chat_instance.send_message( self.user_id, self.first_name, message_content)
             self.text.delete("1.0", "end")
             
-                      
-            
+
+
+    # ----------------- party for refresh message ------------------   
+
+    # method for refresh message        
     def refresh_messages(self):
-        # Ajoutez une vérification pour voir si le rafraîchissement doit continuer
         if self.should_refresh_messages and hasattr(self, 'current_chat_instance') and self.current_chat_instance.id_room:
-            self.frame4_message(self.current_chat_instance.id_room)  # Mise à jour des messages
-            self.after(1000, self.refresh_messages)  # Planifiez le prochain rafraîchissement
+            self.frame4_message(self.current_chat_instance.id_room)  
+            self.after(1000, self.refresh_messages)  
             
-            
-    
-            
-    
+    # method for stop refresh message 
     def stop_refreshing_messages(self):
-        # Appelez cette méthode pour arrêter le rafraîchissement
         self.should_refresh_messages = False
 
+
+    # method for start refresh message
     def start_refreshing_messages(self):
-        # Appelez cette méthode pour démarrer ou redémarrer le rafraîchissement
         self.should_refresh_messages = True
         self.refresh_messages()
 
-    
-        
-    
-
-    
-    
 
 if __name__ == "__main__":
         app = MainPage_graph()
